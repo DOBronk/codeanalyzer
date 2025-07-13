ebd650d55f48b388e7005477a372762b9719995a
Dennis Bronk <dbronk2@gmail.com>
2025-07-13 22:30:26 +0200

    Change of variable names in html form so code variables can be inserted more directly. Validation moved to FormRequest

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -2,30 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreIssueRequest;
 use App\Models\Jobitems;
-use App\Models\Jobs;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Jobissues;
 use App\Services\GithubService;
 
 class IssueController extends Controller
 {
-    public function index(GithubService $git)
+    /**
+     * Index page for displaying issues
+     */
+    public function index(): View
     {
         return view('issues.index', ['items' => Jobissues::with('job')->orderByDesc('created_at')->currentUser()->paginate(10)]);
     }
 
-    public function show(Jobissues $jobissues)
+    public function show(Jobissues $jobissues): View
     {
         return view('issues.show', ['item' => $jobissues, 'job' => $jobissues->job]);
     }
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Jobitems $jobitems)
+    public function create(Jobitems $jobitems): View
     {
         if ($jobitems->status_id != 1) {
-            abort(500, 'Onjuiste issue status');
+            abort(422, 'Onjuiste issue status');
         }
         return view('issues.create', ['item' => $jobitems]);
     }
@@ -33,30 +37,17 @@ class IssueController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Jobitems $jobitems, Request $request, GithubService $git)
+    public function store(StoreIssueRequest $request, Jobitems $jobitems,  GithubService $git): RedirectResponse
     {
-        extract($request->validate([
-            'issuetext' => 'required|string',
-            'title' => 'required|string|max:1024'
-        ]));
-
-        $job = Jobs::find($jobitems->job_id);
-
         try {
-            Jobissues::create([
-                'job_id' => $job->id,
-                'jobitem_id' => $jobitems->id,
-                'user_id' => $request->user()->id,
-                'title' => $title,
-                'text' => $issuetext,
-                'git_url' => $git->createIssue($job->owner, $job->repository, $title, $issuetext),
-            ]);
+            $link = $git->createIssue($jobitems->job->owner, $jobitems->job->repository, $request['title'], $request['text']);
+            Jobissues::create(['git_url' => $link, ...$request->validated()]);
         } catch (\Exception $e) {
             return back()->withError($e->getMessage());
         }
 
         $jobitems->update(['status_id' => 3]);
 
-        return redirect()->route('codeanalyzer.job', ['jobs' => $job])->with('message', 'Issue succesvol aangemaakt');
+        return redirect()->route('codeanalyzer.job', ['jobs' => $jobitems->job])->with('message', 'Issue succesvol aangemaakt');
     }
 }
