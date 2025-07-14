4d623822b0f43816d01cfb8ede531cd1a621d866
Dennis Bronk <dbronk2@gmail.com>
2025-07-14 22:54:56 +0200

    Changed terrible function name and changed method of status and error handling

--- app/Services/GithubService.php
+++ app/Services/GithubService.php
@@ -2,32 +2,38 @@
 
 namespace App\Services;
 
-use App\Models\Jobs;
+use Exception;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use App\Exceptions\GithubExceptions\ConflictException;
 use App\Exceptions\GithubExceptions\ValidationException;
 use App\Exceptions\GithubExceptions\ResourceNotFoundException;
 
-use function PHPUnit\Framework\isArray;
+use App\Services\EnumStatus as Status;
 
 class GithubService
 {
+    private const REPO = ['owner', 'repository', 'branch'];
+
     public function __construct(private readonly string $uri, private string $key) {}
 
     /**
      * Function to return an exception based on a HTTP status code
      * @param int $status HTTP Status Code
      */
-    private function handleStatus(int $status): \Exception
+    private function handleStatus(Response $http): Exception|array|null
     {
-        return match ($status) {
-            401 => new \Exception('Login failed, please check your API key settings'),
-            404 => new ResourceNotFoundException('Resource not found!'),
-            409 => new ConflictException('Conflict!'),
-            410 => new \Exception('Issues are disabled for this repository'),
-            422 => new ValidationException('Validation error!'),
-            default => new \Exception('Unkown status code')
+        return match (Status::from($http->status())) {
+            Status::OK => $http->json(),
+            Status::Created => $http->json(),
+            Status::NoContent => null,
+            Status::AuthFailed => new Exception('Login failed, please check your API key settings'),
+            Status::ResourceNotFound => new ResourceNotFoundException('Resource not found!'),
+            Status::Conflict => new ConflictException('Conflict!'),
+            Status::IssuesDisable => new Exception('Issues are disabled for this repository'),
+            Status::ValidationError => new ValidationException('Validation error!'),
+            default => new Exception('Unkown status code')
         };
     }
     /**
@@ -37,24 +43,22 @@ class GithubService
      * @param string $sha Branch name or SHA for branch
      * @return array Returns an array with filepath as key and corresponding SHA
      */
-    public function getPhpFilesFromTree(array|string $value, ?string $repository = null, ?string $branch = 'main'): array
+    public function getRepository(array $values, string $extension = ".php"): array
     {
-        if (!is_array($value)) {
-            $owner = $value;
-        } elseif (count(array_intersect(array_keys($value), ['owner', 'repository', 'branch'])) === 3) {
-            [$owner, $repository,  $branch] = [$value['owner'], $value['repository'], $value['branch']];
+        if (count(array_intersect(array_keys($values), self::REPO)) === 3) {
+            [$owner, $repository,  $branch] = [$values['owner'], $values['repository'], $values['branch']];
         } else {
-            throw new \Exception('Invalid value passed to function');
+            throw new Exception('Invalid value passed to function');
         }
 
         $uri = "{$this->uri}/repos/{$owner}/{$repository}/git/trees/{$branch}";
         $http = Http::withToken($this->key)->get($uri, ['recursive' => 1]);
 
-        if ($http->status() !== 200) {
-            throw $this->handleStatus($http->status());
+        if (($response = $this->handleStatus($http)) instanceof Exception) {
+            throw $response;
         }
 
-        return array_filter($http->json()['tree'], fn($item) => $item['type'] === "blob" && str_ends_with($item['path'], '.php'));
+        return array_filter($response['tree'], fn($item) => $item['type'] === "blob" && str_ends_with($item['path'], $extension));
     }
 
     /**
@@ -64,22 +68,16 @@ class GithubService
      * @param string $sha SHA code for blob
      * @return string|null Return blob as string or null on failure
      */
-    public function getBlob(string $owner, string $repository, string $sha, string $api = ''): string|null
+    public function getBlob(string $owner, string $repository, string $sha, ?string $api = null): ?string
     {
-        if ($api != '') {
-            $this->key = $api;
-        }
-
         $uri = "{$this->uri}/repos/{$owner}/{$repository}/git/blobs/{$sha}";
-        $http =  Http::withToken($this->key)->get($uri);
-        Log::info("Blob: {$sha} http: {$http}");
+        $http =  Http::withToken($api ?? $this->key)->get($uri);
 
-        $json = $http->json();
-        if ($http->status() == 200 && array_key_exists('content', $json)) {
-            return base64_decode($json['content']);
-        } else {
-            throw $this->handleStatus($http->status());
+        if (($response = $this->handleStatus($http)) instanceof Exception) {
+            throw $response;
         }
+
+        return base64_decode($response['content']);
     }
     /**
      * Summary of createIssue
@@ -92,24 +90,17 @@ class GithubService
     public function createIssue(string $owner, string $repository, string $title, string $body): string
     {
         $uri = "{$this->uri}/repos/{$owner}/{$repository}/issues";
+
         $http = Http::withToken($this->key)->post($uri, [
             'title' => $title,
             'body' => $body,
             'labels' => ['AI Generated Issue']
         ]);
-        dump($http);
-        dd($http->status());
-        if ($http->status() !== 201) {
-            throw $this->handleStatus($http->status());
-        } else {
-            $response = $http->json();
-            Log::info("Issue created");
-            if (array_key_exists('html_url', $response)) {
-                return $response['html_url'];
-            } else {
-                Log::info("No URL on issue created!", $response);
-                return '';
-            }
+
+        if (($response = $this->handleStatus($http)) instanceof Exception) {
+            throw $response;
         }
+
+        return $response['html_url'];
     }
 }
