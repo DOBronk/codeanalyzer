4f9b6f36717e37d0b2b116d022ab61c71ed15673
Dennis Bronk <dbronk2@gmail.com>
2025-07-16 21:08:18 +0200

    Some more pint cleanups

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-
 
         DB::table('codeanalyzer_job_status')->insert([
             'id' => 3,
--- database/seeders/JobstatusSeeder.php
+++ database/seeders/JobstatusSeeder.php
@@ -2,17 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class JobstatusSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-
-    }
+    public function run(): void {}
 }
