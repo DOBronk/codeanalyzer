284301375245a35e4fdef1f0553af94fa6a592fd
Dennis Bronk <dbronk2@gmail.com>
2025-08-14 18:10:39 +0200

    TreeViewer vue component, panels toegevoegd, code opgeschoond (oa interpolation gebruikt) en wat  netter laten werken

--- resources/vue/treeviewer.vue
+++ resources/vue/treeviewer.vue
@@ -1,67 +1,78 @@
 <template>
-  <div class="card max-w-7xl">
-    <form :action="route" method="post">
-      <div class="flex items-center justify-center">
-        <div class="flex flex-col gap-2 w-6/20">
-          <label for="repo">Eigenaar</label>
-          <InputText v-model="owner" :value="owner" name="owner" :invalid="!validOwner" class="w-9/10" />
-        </div>
-        <div class="flex flex-col gap-2 w-6/20">
-          <label for="repo">Repository</label>
-          <Select v-model="selectedRepository" @change="changeBranch" :options="repositories" :placeholder="repository"
-            class="w-9/10" :disabled="!validOwner" />
-          <input type="hidden" name="repository" :value="selectedRepository" />
-        </div>
-        <div class="flex flex-col gap-2 w-5/20">
-          <label for="branch">Branch</label>
-          <Select v-model="selectedBranch" @change="changeTree" :options="cbranch" :placeholder="branch" class="w-9/10"
-            :disabled="!validOwner" />
-          <input type="hidden" name="branch" :value="selectedBranch" />
-        </div>
-        <div class="flex flex-col right-0 w-3/20">
-          <Button class="bg-blue-600 text-white mt-7 py-2 rounded-sm hover:bg-blue-700 transition" severity="info"
-            :disabled="!canSubmit" type="submit">
-            Verzenden
-          </Button>
-        </div>
-      </div>
-      <div class="w-full mt-5 mb-5">
-        <Message v-if="!validOwner" severity="error">
-          Geen geldige repository eigenaar ingevuld
-        </Message>
-        <Message v-else-if="nodes && !canSubmit" severity="info">
-          Selecteer eerst bestanden om verder te gaan
-        </Message>
-        <Message v-else-if="canSubmit" severity="success">
-          Bestanden geselecteerd: {{ countFiles }}
-        </Message>
+    <div class="card max-w-7xl">
+        <form :action="route" method="post">
+            <Panel header="Gegevens">
+                <div class="flex items-center justify-center">
+                    <div class="flex flex-col gap-2 w-6/20">
+                        <label for="owner">Eigenaar</label>
+                        <InputText v-model="owner" name="owner" :invalid="!validOwner" class="w-9/10" />
+                    </div>
 
-      </div>
-      <input type="hidden" name="_token" :value="csrf" />
-      <input type="hidden" name="owner" :value="owner" />
-      <div v-if="nodes == null" class="flex items-center mt-30">
-        <ProgressSpinner />
-      </div>
+                    <div class="flex flex-col gap-2 w-6/20">
+                        <label for="repo">Repository</label>
+                        <Select v-model="selectedRepository" @change="changeBranch" :options="repositories"
+                            :placeholder="repository" class="w-9/10" :disabled="!validOwner" />
+                        <input type="hidden" name="repository" :value="selectedRepository" />
+                    </div>
 
-      <TreeTable v-model:selectionKeys="selectedKey" :value="nodes" v-if="nodes" selectionMode="checkbox"
-        tableStyle="min-width: 50rem" name="test">
-        <template #header>
-          <div class="text-xl font-bold">Github tree bestanden {{ selectedRepository }} {{ selectedBranch }} </div>
-        </template>
-        <Column field="name" header="Naam" expander style="width: 34%"></Column>
-        <Column field="size" header="Grootte" style="width: 33%"></Column>
-        <Column field="type" header="Type" style="width: 33%" sortable></Column>
-      </TreeTable>
+                    <div class="flex flex-col gap-2 w-5/20">
+                        <label for="branch">Branch</label>
+                        <Select v-model="selectedBranch" @change="changeTree" :options="branchOptions"
+                            :placeholder="branch" class="w-9/10" :disabled="!validOwner" />
+                        <input type="hidden" name="branch" :value="selectedBranch" />
+                    </div>
 
-      <template v-for="(x, index) in selectedKey">
-        <template v-if="!index.includes(':folder:')">
-          <input type="hidden" :name="'selections[' + curIndex.toString() + '][path]'" :value="index.split(':')[0]" />
-          <input type="hidden" :name="'selections[' + (curIndex++).toString() + '][sha]'"
-            :value='index.split(":")[1]' />
-        </template>
-      </template>
-    </form>
-  </div>
+                    <div class="flex flex-col right-0 w-3/20">
+                        <Button class="bg-blue-600 text-white mt-7 py-2 rounded-sm hover:bg-blue-700 transition"
+                            severity="info" :disabled="noFolders.length === 0" type="submit">
+                            Verzenden
+                        </Button>
+                    </div>
+                </div>
+
+                <div class="w-full mt-5">
+                    <Message v-if="!validOwner" severity="error">
+                        Geen geldige repository eigenaar ingevuld
+                    </Message>
+                    <Message v-else-if="nodes && noFolders.length === 0" severity="info">
+                        Selecteer eerst bestanden om verder te gaan
+                    </Message>
+                    <Message v-else-if="noFolders.length > 0" severity="success">
+                        Bestanden geselecteerd: {{ noFolders.length }}
+                    </Message>
+                </div>
+            </Panel>
+
+            <input type="hidden" name="_token" :value="csrf" />
+
+            <Panel header="Bestanden" class="mt-5">
+                <div v-if="nodes == null" class="flex items-center mt-5">
+                    <ProgressSpinner />
+                </div>
+
+                <TreeTable v-if="nodes" v-model:selectionKeys="selectedKey" :value="nodes" selectionMode="checkbox"
+                    tableStyle="min-width: 50rem">
+                    <Column field="name" header="Naam" expander style="width: 34%" sortable />
+                    <Column field="size" header="Grootte" style="width: 33%" sortable />
+                    <Column field="type" header="Type" style="width: 33%" sortable />
+
+                    <template #footer>
+                        <div class="flex flex-col right-0 w-3/20">
+                            <Button class="bg-blue-600 text-white mt-7 py-2 rounded-sm hover:bg-blue-700 transition"
+                                severity="info" :disabled="noFolders.length === 0" type="submit">
+                                Verzenden
+                            </Button>
+                        </div>
+                    </template>
+                </TreeTable>
+            </Panel>
+
+            <template v-for="(val, k) in noFolders" :key="k">
+                <input type="hidden" :name="`selections[${k}][path]`" :value="(val.split(':'))[0]" />
+                <input type="hidden" :name="`selections[${k}][sha]`" :value="(val.split(':'))[1]" />
+            </template>
+        </form>
+    </div>
 </template>
 
 <script setup>
@@ -69,129 +80,124 @@ import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import Select from "primevue/select";
 import Message from "primevue/message";
+import Panel from "primevue/panel";
 import ProgressSpinner from "primevue/progressspinner";
 import TreeTable from "primevue/treetable";
 import Column from "primevue/column";
 import _debounce from "lodash/debounce";
 import { ref, watch, computed } from "vue";
+import axios from "axios";
 
 const props = defineProps(["csrf", "owner", "route"]);
 
-const nodes = ref(null),
-  branches = ref([null]),
-  selectedBranch = ref(),
-  selectedRepository = ref(),
-  selectedKey = ref(),
-  owner = ref(props.owner),
-  repositories = ref(),
-  cbranch = ref();
+const nodes = ref(null);
+const branches = ref([null]);
+const owner = ref(props.owner);
+const repositories = ref();
+const branchOptions = ref();
+const selectedBranch = ref();
+const selectedRepository = ref();
+const selectedKey = ref();
+const defaultBranches = ref([]);
 
-let mainBranches = [];
+const validOwner = computed(() => Array.isArray(repositories.value) && repositories.value.length > 0);
+const selectedIndex = computed(() =>
+    repositories.value?.length > 0
+        ? repositories.value.findIndex((repo) => repo === selectedRepository.value)
+        : 0
+);
+const noFolders = computed(() =>
+    selectedKey.value
+        ? Object.keys(selectedKey.value).filter((key) => !key.includes(":folder:"))
+        : []
+);
+
 let trees = {};
-let curIndex = 0;
 
-const validOwner = computed(() => repositories.value == null ? false : true),
-  canSubmit = computed(() => countFiles.value > 0),
-  selectedIndex = computed(() => repositories.value.length > 0 ? repositories.value.findIndex((repo) => repo == selectedRepository.value) : 0),
-  countFiles = computed(() => {
-    return selectedKey.value ? Object.keys(selectedKey.value).filter((val) => !val.includes('folder')).length : 0;
-  });
-
-// Reset de index van geselecteerde bestanden zodra er een wijziging
-// optreed. Zo begint de v-for van de hidden input field weer correct.
-watch(selectedKey, (newVal) => {
-  curIndex = 0;
-});
-
-watch(owner, (newVal) => {
-  updateModel(newVal);
-});
+watch(owner, _debounce((newVal) => {
+    getRepositories(newVal);
+}, 500));
 
 watch(nodes, (newVal) => {
-  if (newVal == null) {
-    selectedKey.value = null;
-  }
+    if (newVal == null) {
+        selectedKey.value = null;
+    }
 });
 
-const updateModel = _debounce((newVal) => {
-  getRepositories(newVal);
-}, 500);
-
 function changeBranch() {
-  nodes.value = null;
-  let index = selectedIndex.value;
+    const index = selectedIndex.value;
 
-  if (branches.value[index] == null) {
-    axios
-      .post("/getbranches", {
-        owner: owner.value,
-        repository: repositories.value[index],
-      })
-      .then((response) => {
-        branches.value[index] = response.data;
-        cbranch.value = branches.value[index];
-        selectedBranch.value = mainBranches.value[index];
+    if (!branches.value[index]) {
+        axios
+            .post("/getbranches", {
+                owner: owner.value,
+                repository: repositories.value[index],
+            })
+            .then((response) => {
+                branches.value[index] = response.data;
+                branchOptions.value = branches.value[index];
+                selectedBranch.value = defaultBranches.value[index];
+                changeTree();
+            })
+            .catch(handleError);
+    } else {
+        branchOptions.value = branches.value[index];
+        selectedBranch.value = defaultBranches.value[index];
         changeTree();
-      }).catch((error) => handleError(error));
-  } else {
-    cbranch.value = branches.value[index]
-    selectedBranch.value = mainBranches.value[index];
-    changeTree();
-  }
+    }
 }
 
 function getRepositories(ownerRepo) {
-  nodes.value = null;
-  repositories.value = null;
+    nodes.value = null;
+    repositories.value = null;
 
-  axios.post("/getrepositories", { owner: ownerRepo }).then(function (response) {
-    repositories.value = response.data[0];
-    mainBranches.value = response.data[1];
-
-    selectedRepository.value = repositories.value[0];
-    changeBranch();
-  }).catch((error) => handleError(error))
+    axios
+        .post("/getrepositories", { owner: ownerRepo })
+        .then((response) => {
+            repositories.value = response.data[0];
+            defaultBranches.value = response.data[1];
+            selectedRepository.value = repositories.value[0];
+            changeBranch();
+        })
+        .catch(handleError);
 }
 
 function changeTree() {
-  if ((nodes.value = getIfExists(owner.value, selectedRepository.value, selectedBranch.value, true)) == null) {
+    nodes.value = null;
+
+    const cachedTree = getIfExists(owner.value, selectedRepository.value, selectedBranch.value);
+    if (cachedTree) {
+        nodes.value = cachedTree;
+        return;
+    }
+
     axios
-      .post("/gettree", {
-        owner: owner.value,
-        branch: selectedBranch.value,
-        repository: selectedRepository.value,
-      })
-      .then(function (response) {
-        nodes.value = response.data;
-        saveTree(owner.value, selectedRepository.value, selectedBranch.value);
-      }).catch((error) => handleError(error));
-  }
+        .post("/gettree", {
+            owner: owner.value,
+            branch: selectedBranch.value,
+            repository: selectedRepository.value,
+        })
+        .then((response) => {
+            nodes.value = response.data;
+            saveTree(owner.value, selectedRepository.value, selectedBranch.value);
+        })
+        .catch(handleError);
 }
 
 function getIfExists(own, rep, bra) {
-  if (own in trees && rep in trees[own]) {
-    if (bra in trees[own][rep]) {
-      return trees[own][rep][bra];
-    }
-  }
-  return null;
+    selectedKey.value = null;
+    return trees?.[own]?.[rep]?.[bra] ?? null;
 }
 
 function saveTree(own, rep, bra) {
-  if (own in trees) {
-    if (rep in trees[own]) {
-      trees[own][rep][bra] = nodes.value;
-    } else {
-      trees[own][rep] = { [bra]: nodes.value };
-    }
-  } else {
-    trees[own] = { [rep]: { [bra]: nodes.value } };
-  }
+    trees[own] ??= {};
+    trees[own][rep] ??= {};
+    trees[own][rep][bra] = nodes.value;
 }
 
 function handleError(error) {
-  if (error.response.status == 401) {
-    location.replace(location.protocol + "//" + location.hostname + "/login");
-  }
+    if (error.response?.status === 401) {
+        location.replace(`${location.protocol}//${location.hostname}/login`);
+    }
 }
 </script>
